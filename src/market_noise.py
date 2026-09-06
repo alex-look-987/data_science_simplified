6864b32038b85922d8376cdfa723cb9db705cc69
alex-look-987 <sqrpshr.23@gmail.com>
2026-09-06 11:13:57 -0600

    chore: testing black formating

--- src/market_noise.py
+++ src/market_noise.py
@@ -3,12 +3,14 @@ import numpy as np
 from helper import load_config, create_parent_directory
 from omegaconf import DictConfig
 
+
 def read_data(config: DictConfig):
     return pd.read_csv(config.interim.path)
 
+
 def simulate_gaussian_noise(df: pd.DataFrame, config: DictConfig) -> pd.DataFrame:
     """Simulation market data variability adding gaussian noise
-    
+
     Args:
         df (pd.DataFrame): _description_
         config (_type_): _description_
@@ -16,10 +18,13 @@ def simulate_gaussian_noise(df: pd.DataFrame, config: DictConfig) -> pd.DataFram
     Returns:
         pd.DataFrame: _description_
     """
-    
-    noise = np.random.normal(config.market_noise.loc, config.market_noise.scale, size=df.shape)
+
+    noise = np.random.normal(
+        config.market_noise.loc, config.market_noise.scale, size=df.shape
+    )
 
     return df + noise
+
 
 def simulate_data_drift(df: pd.DataFrame, config: DictConfig) -> pd.DataFrame:
     """Simulates data drift distributiom between runs
@@ -32,13 +37,17 @@ def simulate_data_drift(df: pd.DataFrame, config: DictConfig) -> pd.DataFrame:
         pd.DataFrame: _description_
     """
 
-    shift = np.random.uniform(-config.market_noise.drift_factor, config.market_noise.drift_factor)
+    shift = np.random.uniform(
+        -config.market_noise.drift_factor, config.market_noise.drift_factor
+    )
 
     return df * (1 + shift)
+
 
 def save_processed_data(df: pd.DataFrame, config: DictConfig):
     create_parent_directory(config.processed.path)
     df.to_csv(config.processed.path, index=False)
+
 
 def processed_market_noise():
     config = load_config()
@@ -49,6 +58,7 @@ def processed_market_noise():
     df = simulate_data_drift(df, config)
 
     save_processed_data(df, config)
+
 
 if __name__ == "__main__":
     processed_market_noise()
--- src/process_data.py
+++ src/process_data.py
@@ -2,17 +2,19 @@ import pandas as pd
 from helper import load_config, create_parent_directory
 from omegaconf import DictConfig
 
+
 def read_data(config: DictConfig):
-    return pd.read_csv(config.raw_data.path, index_col='date')
+    return pd.read_csv(config.raw_data.path, index_col="date")
+
 
 def feature_engineering(df: pd.DataFrame) -> pd.DataFrame:
-    return df.assign(
-        body=abs(df['open'] - df['close'])
-    )
+    return df.assign(body=abs(df["open"] - df["close"]))
+
 
 def save_processed_data(df: pd.DataFrame, config: DictConfig):
     create_parent_directory(config.interim.path)
     df.to_csv(config.interim.path, index=False)
+
 
 def process_data():
     config = load_config()
@@ -20,6 +22,7 @@ def process_data():
     df = feature_engineering(df)
 
     save_processed_data(df, config)
+
 
 if __name__ == "__main__":
     process_data()
